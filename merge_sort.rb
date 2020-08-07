efc1b05c6c9ef5f6db0947a4b078a3e78a88dfc3
BernH4 <bernhard.steidl14mail.com>
2020-08-07 10:35:07 +0200

    Huge refactor -> code alot cleaner now

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,36 +1,21 @@
-require 'pry'
-rand_arr = Array.new(100) { rand 500 }
-# rand_arr = [8,5,9,7]
+rand_arr = Array.new(101) { rand 999 }
 def merge_sort(arr)
-  left_half  = arr[0, arr.length / 2]
-  right_half = arr[arr.length / 2..-1]
-  if arr.length > 1
-    left_half = arr[0, arr.length / 2] = merge_sort(left_half)
-    right_half = arr[arr.length / 2..-1] = merge_sort(right_half)
-    left_i = 0
-    right_i = 0
-    new_arr = []
-    # Merge process
-    loop do
-      if left_half[left_i] < right_half[right_i]
-        new_arr << left_half[left_i]
-        left_i += 1
-        if left_i == left_half.length
-          new_arr.push(*right_half[right_i..-1])
-          break
-        end
-      else
-        new_arr << right_half[right_i]
-        right_i += 1
-        if right_i == right_half.length
-          new_arr.push(*left_half[left_i..-1])
-          break
-        end
-      end
+  return arr unless arr.length > 1
+  mid = arr.length / 2
+  left_half = merge_sort(arr[0, mid])
+  right_half = merge_sort(arr[mid..-1])
+
+  # Merge process
+  sorted_arr = []
+  until left_half.empty? || right_half.empty?
+    if left_half[0] < right_half[0]
+      sorted_arr.push(left_half.shift)
+    else
+      sorted_arr.push(right_half.shift)
     end
-    new_arr
-  else
-    arr
   end
+  # push the remaining sorted items of the non empty arr into the sorted arr
+  sorted_arr.concat(left_half, right_half)
 end
-puts "final result: \e[31m#{merge_sort(rand_arr)}\e[0m"
+
+p merge_sort(rand_arr)
